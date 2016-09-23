4c31d7a52cdb6987f6e207924895b114ed9244c9
Ashish Jacob <ashishjohnson.jacob@my.jcu.edu.au>
2016-09-23 12:40:51 +0800

    Updated Login

--- logincheck.php
+++ logincheck.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 session_start();
 
 ?>
 <!DOCTYPE HTML>
-<html>  
+<html>
 <body style=" background-image:url(bg.jpg);
     background-repeat: no-repeat;
     background-attachment: fixed;
@@ -16,13 +16,21 @@ require "databaseConnection.php";
 $user = strval($_POST["name"]);
 $pass = strval($_POST["password"]);
 
-$_SESSION["valid"] = authorization($user, $pass);
+$result = authorization($user, $pass);
 
-if($_SESSION["valid"] == "Admin"){
-	header("Location: employee.php");
-}
-else{
-	echo " <div align='center'> <h1>Wrong username or password!</h1><p> <br><a href='index.php'>Return</a></div></p> ";
+$_SESSION["valid"] = $result["token"];
+$_SESSION["loginID"] = $result["id"];
+
+switch ($_SESSION["valid"]){
+
+	case "Admin":
+		header("Location: employee.php");
+		break;
+	case "User":
+		header("Location: test.php");
+		break;
+	default:
+		echo " <div align='center'> <h1>Wrong username or password!</h1><p> <br><a href='index.php'>Return</a></div></p> ";
 }
 
 ?>
